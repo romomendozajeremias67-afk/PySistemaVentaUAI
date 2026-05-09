2ad8e5b0d5cfa45d65b2f9ab6e2de3be673333e4
Jeremias Romo Mendoza <romomendozajeremias67@gmail.com>
2026-05-08 19:13:11 -0500

    Primer commit del proyecto

---
+++ src/main/java/com/mycompany/pysistemaventauai/PySistemaVentaUAI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pysistemaventauai;
+
+/**
+ *
+ * @author INNOVA
+ */
+public class PySistemaVentaUAI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
